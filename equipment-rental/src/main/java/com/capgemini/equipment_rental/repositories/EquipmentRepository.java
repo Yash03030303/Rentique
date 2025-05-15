1ff78c86a2e1f7ee6d0bd3dbe5787e34bd7659d6
Yash03030303 <yashdere03@gmail.com>
2025-05-15 12:02:38 +0530

    New commit 2

--- equipment-rental/src/main/java/com/capgemini/equipment_rental/repositories/EquipmentRepository.java
+++ equipment-rental/src/main/java/com/capgemini/equipment_rental/repositories/EquipmentRepository.java
@@ -8,5 +8,5 @@ import com.capgemini.equipment_rental.entity.Equipment;
 @Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
 
-	boolean existsByName(String name);//
+	boolean existsByName(String name);
 }
