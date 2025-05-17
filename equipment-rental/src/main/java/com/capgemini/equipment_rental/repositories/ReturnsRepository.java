02f5192376db2d57a49e0f321abaecaa32c24553
Yash Dere <119395742+Yash03030303@users.noreply.github.com>
2025-05-17 16:49:26 +0530

    search endpoint added (#25)
    Co-authored-by: Krish-Mahorkar <krishmahorkar20@gmail.com>

--- equipment-rental/src/main/java/com/capgemini/equipment_rental/repositories/ReturnsRepository.java
+++ equipment-rental/src/main/java/com/capgemini/equipment_rental/repositories/ReturnsRepository.java
@@ -1,5 +1,9 @@
 package com.capgemini.equipment_rental.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,6 @@ import com.capgemini.equipment_rental.entity.Returns;
 
 @Repository
 public interface ReturnsRepository extends JpaRepository<Returns, Long> {
-
+	Page<Returns> findByItemConditionContainingIgnoreCaseAndReturnDateBetween(
+		    String itemCondition, LocalDate startDate, LocalDate endDate, Pageable pageable);
 }
