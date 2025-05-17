5e3fa12027afb6cb261234ebe19740267789a423
Yash03030303 <yashdere03@gmail.com>
2025-05-17 16:13:53 +0530

    Supplier dashboard added

--- equipment-rental/src/main/java/com/capgemini/equipment_rental/repositories/RentalsRepository.java
+++ equipment-rental/src/main/java/com/capgemini/equipment_rental/repositories/RentalsRepository.java
@@ -1,9 +1,16 @@
 package com.capgemini.equipment_rental.repositories;
 
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.capgemini.equipment_rental.entity.Rentals;
 
 public interface RentalsRepository extends JpaRepository<Rentals, Long> {
-}
+	@Query("SELECT COUNT(r) FROM Rentals r " + "WHERE NOT EXISTS (SELECT ret FROM Returns ret WHERE ret.rental = r)")
+	Long countActiveRentals();
 
+	@Query("SELECT FUNCTION('DATE_FORMAT', r.rentalDate, '%Y-%m'), COUNT(r) "
+			+ "FROM Rentals r GROUP BY FUNCTION('DATE_FORMAT', r.rentalDate, '%Y-%m')")
+	List<Object[]> getMonthlyRentals();
+}
